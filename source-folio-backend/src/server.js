9113265dd70041b28a78d2728ee8e6308881ad82
Sumit <vermasumit923@gmail.com>
2023-03-25 23:29:52 +0530

    inserted users in cloudinary-setup

--- source-folio-backend/src/server.js
+++ source-folio-backend/src/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import {users} from "../users.js";
 const app = express(); 
+app.set('view engine', 'ejs');
 app.use(express.json());
 app.get('/hello', (req, res) => {
     res.send("hello");
@@ -7,8 +9,8 @@ app.get('/hello', (req, res) => {
 
 app.get('/user/:id', (req, res) => {
     const id = req.params.id;
-    res.send(`Hello ${id}`);
-})
+    res.send(`Hello ${users[id]["name"]}`);
+});
 
 app.listen(8000, () => {
     console.log('server is listening on port:8000');
