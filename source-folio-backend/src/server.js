0eba7f66700cce70a4874b6b8ddc203cbad9a4ee
Sumit <vermasumit923@gmail.com>
2023-04-01 01:23:06 +0530

    created an API for pushing data into mongo

--- source-folio-backend/src/server.js
+++ source-folio-backend/src/server.js
@@ -1,16 +1,291 @@
 import express from 'express';
-import {users} from "../users.js";
+import path from 'path';
+import mongoose from 'mongoose';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express(); 
-app.set('view engine', 'ejs');
-app.use(express.json());
-app.get('/hello', (req, res) => {
-    res.send("hello");
+
+const dbUrl = 'mongodb://localhost:27017/source-folio';
+mongoose.connect(dbUrl);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("connection open");
 });
 
-app.get('/user/:id', (req, res) => {
-    const id = req.params.id;
-    res.send(`Hello ${users[id]["name"]}`);
+function convertJSON(inputJSON) {
+    
+    const outputJSON = {
+    	"name": "",
+    
+    	"mainDesignations": [],
+    
+    	"description": "",
+    	"profilePicture": "",
+    
+    	"myEducation": [],
+    
+    	"myExperience": [],
+    
+    	"myProjects": [],
+    
+    	"mySkills": {
+    		"programmingSkills": [],
+    		"toolsAndFrameworks": []
+    	},
+    
+    	"myAchievements": [],
+    	"linkedIn": "",
+    	"instagram": "",
+    	"telephone": "",
+    	"email": ""
+    };
+    
+    outputJSON['name'] = inputJSON['name'];
+    if(typeof(inputJSON['mainDesignations']) == 'object') outputJSON['mainDesignations'] = inputJSON['mainDesignations'];
+    else outputJSON['mainDesignations'].push(inputJSON['mainDesignations']);
+    outputJSON['description'] = inputJSON['description'];
+    outputJSON['profilePicture'] = inputJSON['profilePicture'];
+    outputJSON['linkedIn'] = inputJSON['linkedIn'];
+    outputJSON['instagram'] = inputJSON['instagram'];
+    outputJSON['telephone'] = inputJSON['telephone'];
+    outputJSON['email'] = inputJSON['email'];
+    if(typeof(inputJSON['myAchievements']) == 'object') outputJSON['myAchievements'] = inputJSON['myAchievements'];
+    else outputJSON['myAchievements'].push(inputJSON['myAchievements']);
+
+    if(typeof(inputJSON['institutionName']) == 'object') {
+        for(let i = 0; i < inputJSON['institutionName'].length; i++) {
+            const obj = {
+                "institutionName": "",
+                "place": "",
+                "year": 0,
+                "aggregate": 0,
+                "coursePursuied": ""
+            };
+            obj['institutionName'] = inputJSON['institutionName'][i];
+            obj['place'] = inputJSON['place'][i];
+            obj['year'] = Number(inputJSON['year'][i]);
+            obj['aggregate'] = Number(inputJSON['aggregate'][i]);
+            obj['coursePursuied'] = inputJSON['coursePursuied'][i];
+            
+            outputJSON['myEducation'].push(obj);
+        }
+    } else {
+        const obj = {
+            "institutionName": "",
+            "place": "",
+            "year": 0,
+            "aggregate": 0,
+            "coursePursuied": ""
+        };
+        obj['institutionName'] = inputJSON['institutionName'];
+        obj['place'] = inputJSON['place'];
+        obj['year'] = Number(inputJSON['year']);
+        obj['aggregate'] = Number(inputJSON['aggregate']);
+        obj['coursePursuied'] = inputJSON['coursePursuied'];
+        outputJSON['myEducation'].push(obj);
+    }
+    
+    if(typeof(inputJSON['role']) == 'object') {
+        for(let i = 0; i < inputJSON['role'].length; i++) {
+            const obj = {
+                "role": "",
+                "duration": {
+                    "start": "",
+                    "end": ""
+                },
+                "company": "",
+                "workDescription": [],
+                "certificate": ""
+            };
+            
+            obj["role"] = inputJSON["role"][i];
+            obj["company"] = inputJSON["company"][i];
+            obj["certificate"] = inputJSON["certificate"][i];
+            obj["duration"]["start"] = inputJSON["start"][i];
+            obj["duration"]["end"] = inputJSON["end"][i];
+            if(typeof(inputJSON['workDescription_'+i]) == 'object') obj["workDescription"] = inputJSON["workDescription_" + i];
+            else obj['workDescription'].push(inputJSON['workDescription_'+i]);
+            outputJSON["myExperience"].push(obj);
+        }
+    } else {
+        const obj = {
+            "role": "",
+            "duration": {
+                "start": "",
+                "end": ""
+            },
+            "company": "",
+            "workDescription": [],
+            "certificate": ""
+        };
+
+        obj["role"] = inputJSON["role"];
+        obj["company"] = inputJSON["company"];
+        obj["certificate"] = inputJSON["certificate"];
+        obj["duration"]["start"] = inputJSON["start"];
+        obj["duration"]["end"] = inputJSON["end"];
+        if(typeof(inputJSON['workDescription_0']) == 'object') obj["workDescription"] = inputJSON["workDescription_0"];
+        else obj['workDescription'].push(inputJSON['workDescription_0']);
+        outputJSON["myExperience"].push(obj);
+    }
+    
+    if(typeof(inputJSON['projectName']) == 'object') {
+        for(let i = 0; i < inputJSON['projectName'].length; i++) {
+            const obj = {
+                "projectName": "",
+                "description": [],
+                "gitHubLink": "",
+                "projectLink": "",
+            };
+            
+            obj["projectName"] = inputJSON["projectName"][i];
+            obj["description"] = inputJSON["projectDescription_" + i];
+            obj["gitHubLink"] = inputJSON["githubLink"][i];
+            obj["projectLink"] = inputJSON["projectLink"][i];
+            
+            outputJSON["myProjects"].push(obj);
+        }
+    } else {
+        const obj = {
+            "projectName": "",
+            "description": [],
+            "gitHubLink": "",
+            "projectLink": "",
+        };
+
+        obj["projectName"] = inputJSON["projectName"];
+        if(typeof(inputJSON['projectDescription_0']) == 'object') obj["description"] = inputJSON["projectDescription_0"];
+        else obj['description'].push(inputJSON['projectDescription_0']);
+        obj["gitHubLink"] = inputJSON["githubLink"];
+        obj["projectLink"] = inputJSON["projectLink"];
+        
+        outputJSON["myProjects"].push(obj);
+    }
+    
+    if(typeof(inputJSON['skillName']) == 'object') {
+        for(let i = 0; i < inputJSON['skillName'].length; i++) {
+            const obj = {
+                "name": "",
+                "level": ""
+            }
+            
+            obj["name"] = inputJSON["skillName"][i];
+            obj["level"] = inputJSON["skillLevel"][i];
+            
+            outputJSON["mySkills"]["programmingSkills"].push(obj);
+        }
+    } else {
+        const obj = {
+            "name": "",
+            "level": ""
+        }
+
+        obj["name"] = inputJSON["skillName"];
+        obj["level"] = inputJSON["skillLevel"];
+            
+        outputJSON["mySkills"]["programmingSkills"].push(obj);
+    }
+    
+    if(typeof(inputJSON['toolName']) == 'object') {
+        for(let i = 0; i < inputJSON['toolName'].length; i++) {
+            const obj = {
+                "name": "",
+                "level": ""
+            }
+            
+            obj["name"] = inputJSON["toolName"][i];
+            obj["level"] = inputJSON["toolLevel"][i];
+            
+            outputJSON["mySkills"]["toolsAndFrameworks"].push(obj);
+        }
+    } else {
+        const obj = {
+            "name": "",
+            "level": ""
+        }
+        
+        obj["name"] = inputJSON["toolName"];
+        obj["level"] = inputJSON["toolLevel"];
+        
+        outputJSON["mySkills"]["toolsAndFrameworks"].push(obj);
+    }
+    
+    return outputJSON;
+}
+
+const Schema = mongoose.Schema;
+const EducationSchema = new Schema({
+    institutionName: String,
+    place: String,
+    year: Number,
+    aggregate: Number,
+    coursePursuied: String
 });
+
+const DurationSchema = new Schema({
+    start: String,
+    end: String
+})
+
+const ExperienceSchema = new Schema({
+    role: String,
+    duration: DurationSchema,
+    company: String,
+    workDescription: [String],
+    certificate: String
+})
+
+const ProjectSchema = new Schema({
+    projectName: String,
+    description: [String],
+    githubLink: String,
+    projectLink: String
+});
+
+const skillElementSchema = new Schema({
+    name: String,
+    level: Number
+});
+
+const SkillsSchema = new Schema({
+    programmingSkills: [skillElementSchema],
+    toolsAndFrameworks: [skillElementSchema]
+});
+
+const portfolioSchema = new Schema({
+    name: String,
+    mainDesignations: [String],
+    description: String,
+    profilePicture: String,
+    myEducation: [EducationSchema],
+    myExperience: [ExperienceSchema],
+    myProjects: [ProjectSchema],
+    mySkills: SkillsSchema,
+    myAchievements: [String],
+    linkedIn: String,
+    email: String,
+    instagram: String,
+    telephone: Number
+});
+
+const Portfolio = mongoose.model('Portfolio', portfolioSchema);
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/portfolio/insert', async (req, res) => {
+    const obj = req.body;
+    const resultantObj = convertJSON(obj);
+    const mongooseObj = new Portfolio(resultantObj);
+    await mongooseObj.save();
+    res.send(resultantObj);
+})
 
 app.listen(8000, () => {
     console.log('server is listening on port:8000');
