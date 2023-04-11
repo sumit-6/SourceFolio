df470be36f4aa7106ea5734212fb3cdc836d3585
Sumit <vermasumit923@gmail.com>
2023-04-12 00:09:14 +0530

    included firebase auth in backend

--- source-folio-backend/src/server.js
+++ source-folio-backend/src/server.js
@@ -1,19 +1,25 @@
 import express from 'express';
 import path from 'path';
 import mongoose from 'mongoose';
-import helmet from 'helmet';
 import cl from 'cloudinary';
 import multer from 'multer';
 import dotenv from 'dotenv';
-import session from 'express-session';
-import flash from 'connect-flash';
-import MongoDBStorePackage from 'connect-mongodb-session';
+import { fileURLToPath } from 'url';
 import portfolioSchema from '../JoiSchemas.js';
 import ExpressError from '../ExpressError.js';
-
+import fs from 'fs';
+import admin from 'firebase-admin';
 if(process.env.NODE_ENV !== 'production') {
     dotenv.config();
 }
+const credentials = JSON.parse(
+    fs.readFileSync('./credentials.json')
+);
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials),
+});
+
 
 const cloudinary = cl.v2;
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
@@ -30,77 +36,12 @@ const storage = new CloudinaryStorage({
     }
 })
 const upload = multer({storage});
-import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express(); 
 
-const secret = 'thisisasecret';
 
-app.use(helmet.crossOriginOpenerPolicy());
-app.use(helmet.crossOriginResourcePolicy());
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.expectCt());
-app.use(helmet.frameguard());
-app.use(helmet.hidePoweredBy());
-app.use(helmet.hsts());
-app.use(helmet.ieNoOpen());
-app.use(helmet.noSniff());
-app.use(helmet.originAgentCluster());
-app.use(helmet.permittedCrossDomainPolicies());
-app.use(helmet.referrerPolicy());
-app.use(helmet.xssFilter());
-const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://api.mapbox.com/",
-    "https://kit.fontawesome.com/",
-    "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net/",
-];
-const styleSrcUrls = [
-    "https://kit-free.fontawesome.com/",
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.mapbox.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/",
-    "https://cdn.jsdelivr.net/"
-];
-const connectSrcUrls = [
-    "https://api.mapbox.com/",
-    "https://a.tiles.mapbox.com/",
-    "https://b.tiles.mapbox.com/",
-    "https://events.mapbox.com/",
-];
-const fontSrcUrls = [];
-const cloudinary_val = process.env.CLOUDINARY_CLOUD_NAME
-app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: [],
-            connectSrc: ["'self'", ...connectSrcUrls],
-            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-            workerSrc: ["'self'", "blob:"],
-            objectSrc: [],
-            imgSrc: [
-                "'self'",
-                "blob:",
-                "data:",
-                `https://res.cloudinary.com/${cloudinary_val}/`, //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-                "https://images.unsplash.com/",
-            ],
-            fontSrc: ["'self'", ...fontSrcUrls],
-        }
-    })
-);
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
 const dbUrl = 'mongodb://localhost:27017/source-folio';
 mongoose.connect(dbUrl);
@@ -392,42 +333,29 @@ app.set('views', path.join(__dirname, '../views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-
-const MongoDBStore = MongoDBStorePackage(session);
-const store = new MongoDBStore({
-    uri : dbUrl,
-    secret: secret,
-    touchAfter: 24 * 60 * 60
-});
-
-store.on('error', function(error) {
-    console.log("Session Store Error", error);
-})
-app.use(session({
-    store,
-    name: 'session',
-    secret: secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        //secure: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}));
-
-app.use(flash());
-app.use((req, res, next) => {
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
+app.use(async (req, res, next) => {
+    const { authtoken } = req.headers;
+    console.log(authtoken);
+    if(authtoken) {
+        try {
+            req.user = await admin.auth().verifyIdToken(authtoken);
+        }
+        catch (e) {
+            return res.sendStatus(400);
+        }
+    }
+    req.user = req.user || {};
+    next();
+});
 
 app.get('/api/portfolio/:id', async(req, res) => {
     const id = req.params.id;
     const data = await Portfolio.findById(id);
-    console.log(data.myExperience.duration);
     res.json(data);
 });
 
@@ -448,8 +376,8 @@ app.post('/portfolio/edit/:id', async(req, res) => {
     const resultantObj = convertJSON(updatedData);
     validatePortfolio(resultantObj);
     await Portfolio.findByIdAndUpdate(id, resultantObj, {new: true});
-    req.flash('success', 'Successfully Updated!');
-    res.redirect(`http://localhost:3000/portfolio?success=${encodeURIComponent(req.flash('success'))}`);
+    const message = "Successfully Updated!!";
+    res.redirect(`http://localhost:3000/portfolio?success=${message}`);
 })
 
 app.get('/portfolio/delete/:id', async(req, res) => {
