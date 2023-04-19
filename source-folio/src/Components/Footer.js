ca5e011444888c925e589f59e65754cdd8b81a0a
Sumit <vermasumit923@gmail.com>
2023-04-19 20:53:15 +0530

    Delete My Portflio api request added

--- source-folio/src/Components/Footer.js
+++ source-folio/src/Components/Footer.js
@@ -3,6 +3,7 @@ import "./footer.css"
 import { AiOutlineHome } from "react-icons/ai";
 import useUser from "../hooks/useUser";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Footer=(props)=>{
   const {user, isLoading} = useUser();
@@ -13,12 +14,30 @@ const Footer=(props)=>{
       const t = user && await user.getIdToken();
       setToken(t);
     })();
+
+    
   }, [user])
+
+  const handleDelete = async (e) => {
+      e.preventDefault();
+      const config = {
+        headers: {
+          'authtoken': token
+        }
+      }
+    
+      const response = await axios.post(`https://source-folio-backend.onrender.com/portfolio/delete/${props.id}`, {}, config);
+      if(response.data === "Success") {
+        window.location.href = `https://source-folio-frontend.vercel.app/`;
+      } else {
+        window.location.href = `https://source-folio-frontend.vercel.app/pageDoesn'tExist`;
+      }
+    }
     return (
       <section className="footer__copy">
         <div className="footer__content">
           {!isLoading && user && token && (user.uid == props.data.user_id) && <a className="buttonn" onClick={()=>{window.location.href=`https://react-form-ten-steel.vercel.app/form?q=${token}&sfid=${props.id}&where=edit`}}>Edit SourceFolio</a>}
-          {!isLoading && user && token && (user.uid == props.data.user_id) && <a className="delete buttonn">Delete SourceFolio</a>}
+          {!isLoading && user && token && (user.uid == props.data.user_id) && <a className="delete buttonn" onClick={handleDelete}>Delete SourceFolio</a>}
         </div>
         {!isLoading && user && <div className="back__home ">
           <a className="buttonn" onClick={() => {navigate("/")}}>Back to home</a>
