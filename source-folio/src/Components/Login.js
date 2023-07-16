380e897ff205dcfc59320148bf9ad5e009edc667
Amarnath Yadav <amar312696@gmail.com>
2023-07-16 23:05:22 +0530

    Revert "added login and signup  error field"
    This reverts commit d1eb42184074d4ffb9da6bd8a7dad275584a0d2a.

--- source-folio/src/Components/Login.js
+++ source-folio/src/Components/Login.js
@@ -14,10 +14,6 @@ const Login = () => {
   const logIn = async (e) => {
     e.preventDefault();
     try {
-      if(!email ||!password){
-        setError("All fields are mandatory");
-        return;
-      }
       await signInWithEmailAndPassword(getAuth(), email, password);
       navigate("/");
     } catch (err) {
@@ -28,10 +24,6 @@ const Login = () => {
   const createAccount = async (e) => {
     e.preventDefault();
     try {
-      if(!email ||!password ||!confirmPassword){
-        setError("All fields are mandatory");
-        return;
-      }
       if (password !== confirmPassword) {
         setError("Password and confirm password do not match");
         return;
@@ -44,15 +36,72 @@ const Login = () => {
     }
   };
 
-  const clear=()=>{
-    setError("");
-  }
+  /*return (
+    <>
+      <div
+        className="card my-5 shadow"
+        style={{
+          borderRadius: "15px",
+          marginLeft: "2rem",
+          marginRight: "2rem",
+        }}
+      >
+        <div className="container py-4">
+          <div className="text-center">
+            <h1 className="heading1">
+              <b>
+                <u>WELCOME</u>
+              </b>
+            </h1>
+            {error && <p className="error">{error}</p>}
+            <form>
+              <div className="form-group loginsign">
+                <label htmlFor="exampleInputEmail1" className="form-label">
+                  Email address:
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Your email address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></input>
+              </div>
+              <div className="form-group loginsign">
+                <label htmlFor="exampleInputPassword1" className="form-label">
+                  Your Password:
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button
+                onClick={(e) => logIn(e)}
+                className="btn btn-primary submitbutton"
+              >
+                Log In
+              </button>
+            </form>
+            <Link to="/create-account">
+              Don't have an account? Create one here
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );*/
 
-  
   return (
     <div className="form-body">
     <div className="form_main">
-        <input type="checkbox" id="chk" aria-hidden="true" onClick={clear}/>
+        <input type="checkbox" id="chk" aria-hidden="true" />
         <div className="signup">
             <form>
                 <label for="chk" aria-hidden="true" className='form__label'>Login</label>
@@ -60,7 +109,6 @@ const Login = () => {
                 value={email} onChange={(e) => setEmail(e.target.value)} required />
                 <input type="password" name="password" placeholder="password" className="form__input" 
                 value={password} onChange={(e) => setPassword(e.target.value)} required />
-                {error!==""?<div className="error">*{error}</div>:""}
                 <button className="form__button" onClick={logIn}>Login</button>
                 
             </form>
@@ -74,7 +122,6 @@ const Login = () => {
                   onChange={(e) => setPassword(e.target.value)} required />
                 <input type="password" name="re-pswd" placeholder="re-enter password" className="form__input"
                 value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
-                {error!==""?<div className="error">*{error}</div>:""}
                 <button className="form__button" onClick={createAccount}>Sign Up</button>
             </form>
 
