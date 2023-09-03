33cac2c8de578db34920434d0e37ecf83d070669
Devanshi <devanshigupta2002@gmail.com>
2023-09-03 22:12:48 +0530

    feat/formEdit

---
+++ source-folio/src/Components/Form-Components/BioForm.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+const BioForm=()=>{
+    return (
+      <div className="bg-gradient-to-r from-slate-300 to-slate-500 p-4 mt-6">
+        <div>Bio Details</div>
+        <div class="md:flex md:items-center mb-6">
+          <div class="md:w-1/3">
+            <label
+              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              for="inline-full-name"
+            >
+              Full Name
+            </label>
+          </div>
+          <div class="md:w-2/3">
+            <input
+              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="inline-full-name"
+              type="text"
+            />
+          </div>
+        </div>
+        <div class="md:flex md:items-center mb-6">
+          <div class="md:w-1/3">
+            <label
+              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              for="inline-full-name"
+            >
+              Instagram
+            </label>
+          </div>
+          <div class="md:w-2/3">
+            <input
+              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="inline-full-name"
+              type="text"
+            />
+          </div>
+        </div>
+        <div class="md:flex md:items-center mb-6">
+          <div class="md:w-1/3">
+            <label
+              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              for="inline-full-name"
+            >
+              Linked In
+            </label>
+          </div>
+          <div class="md:w-2/3">
+            <input
+              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="inline-full-name"
+              type="text"
+            />
+          </div>
+        </div>
+        <div class="md:flex md:items-center mb-6">
+          <div class="md:w-1/3">
+            <label
+              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              for="inline-full-name"
+            >
+              Mobile Number
+            </label>
+          </div>
+          <div class="md:w-2/3">
+            <input
+              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="inline-full-name"
+              type="number"
+            />
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default BioForm;
--- source-folio/src/Components/Form-Components/Form.js
+++ source-folio/src/Components/Form-Components/Form.js
@@ -1,11 +1,43 @@
 import React from "react";
+import BioForm from "./BioForm";
 
 const Form=()=>{
-    return(
-        <div>
-            <div className="text-2xl text-center text-white">React components</div>
+    return (
+      <div className="lol p-8">
+        <div className="text-2xl text-center text-white">
+          Fill your details below!!
         </div>
-    )
+        <div className="bg-grey-200 border-gray-500 border h-full w-full mt-10 rounded-lg p-8">
+          <div className="flex">
+            <div className="navButton text-xs border h-8 w-24 border-white text-white flex items-center justify-center rounded-2xl cursor-pointer">
+              Bio
+            </div>
+            <div className="navButton text-xs border h-8 w-24 border-white text-white flex items-center justify-center ml-7 rounded-2xl cursor-pointer ">
+              About me
+            </div>
+            <div className="navButton text-xs border h-8 w-32 border-white text-white flex items-center justify-center ml-7 rounded-2xl cursor-pointer">
+              My Education
+            </div>
+            <div className="navButton text-xs border h-8 w-32 border-white text-white flex items-center justify-center ml-7 rounded-2xl cursor-pointer">
+              My Experience
+            </div>
+            <div className="navButton text-xs border h-8 w-24 border-white text-white flex items-center justify-center ml-7 rounded-2xl cursor-pointer">
+              My Projects
+            </div>
+            <div className="navButton text-xs border h-8 w-24 border-white text-white flex items-center justify-center ml-7 rounded-2xl cursor-pointer ">
+              My Skills
+            </div>
+            <div className="navButton text-xs border h-8 w-36 border-white text-white flex items-center justify-center ml-7 rounded-2xl cursor-pointer ">
+              My Achivements
+            </div>
+            <div className="navButton text-xs border h-8 w-24 border-white text-white flex items-center justify-center ml-7 rounded-2xl cursor-pointer">
+              Contact
+            </div>
+          </div>
+          <BioForm/>
+        </div>
+      </div>
+    );
 }
 
 export default Form;
