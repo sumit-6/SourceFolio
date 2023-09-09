6d59561a83b38ae0151556fecc2c3b7550d6e15e
Devanshi <devanshigupta2002@gmail.com>
2023-09-09 12:04:54 +0530

    added/plusButton

--- source-folio/src/Components/Form-Components/AchievementsForm.js
+++ source-folio/src/Components/Form-Components/AchievementsForm.js
@@ -1,12 +1,20 @@
 import React from "react";
 import TextArea from "./TextArea";
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 const AchievementsForm = () => {
     return (
-        <div className="bg-gradient-to-r from-slate-300 to-slate-500 p-4 mt-6">
-            <div>Achievements Details</div>
-            <TextArea field="Achievement" id="myAchievements_0" placeholder="Enter your achievement"></TextArea>
+      <div className="bg-gradient-to-r from-slate-300 to-slate-500 p-4 mt-6 flex justify-around">
+        <div className=" w-2/3">Achievements Details</div>
+        <div className="w-full grid grid-flow-col">
+          <TextArea
+            field="Achievement"
+            id="myAchievements_0"
+            placeholder="Enter your achievement"
+          ></TextArea>
+          <IoIosAddCircleOutline className="h-8 w-8 text-black" />
         </div>
+      </div>
     );
 };
 
--- source-folio/src/Components/Form-Components/TextArea.js
+++ source-folio/src/Components/Form-Components/TextArea.js
@@ -4,7 +4,7 @@ const TextArea = (props) => {
     const {field, type, id, placeholder="", value=""} = props;
     return (
         <div class="md:flex md:items-center mb-6">
-          <div class="md:w-1/3">
+          <div >
             <label
               class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
               htmlFor={id}
@@ -12,9 +12,9 @@ const TextArea = (props) => {
               {field}
             </label>
           </div>
-          <div class="md:w-2/3">
+          <div className="w-full">
             <textarea 
-              rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
+              rows="4" class="w-full block p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
               id={id}
               type={type}
               name={id}
