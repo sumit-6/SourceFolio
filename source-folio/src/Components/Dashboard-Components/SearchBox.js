66865c8c4d60a3925d7fff078ccc2443fd0b1f3e
Sumit <vermasumit923@gmail.com>
2023-10-22 12:07:46 +0530

    feat: Search feature implement

---
+++ source-folio/src/Components/Dashboard-Components/SearchBox.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+
+function SearchBox(props) {
+    const [searchText, setSearchText] = useState("");
+    const handleChange = (e) => {
+        const text = e.target.value;
+        setSearchText(text);
+        props.handleInputChange(text);
+    }
+
+    return (
+        <>
+            <form className="relative w-max mx-auto">
+                <input type="search" name="search" id="search"
+                 className="relative peer z-10 bg-transparent w-12 h-8 rounded-full border focus:w-full
+                  focus:border-orange-300 focus:cursor-text focus:pl-16 focus:pr-4
+                  pl-12
+                  cursor-pointer outline-none" value={searchText}
+                  onChange={(e) => {handleChange(e)}}></input>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="absolute inset-y-0 my-auto w-12 h-8 px-4 stroke-white border-r border-transparent peer-focus:stroke-orange-300 peer-focus:border-orange-300">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                </svg>
+            </form>
+        </>
+    );
+}
+
+export default SearchBox;
