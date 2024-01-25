1e015f7ee618c4b44ea4f1d40056ae7c73c02128
Sumit <vermasumit923@gmail.com>
2024-01-25 16:29:07 +0530

    added a preview feature of sourcefolio

---
+++ source-folio/src/Components/Preview/Preview.js
@@ -0,0 +1,73 @@
+import React, { useEffect } from "react";
+import NavBar from "../NavBar.js";
+import Home from "../Home.js";
+import "../../App.css";
+import Aboutme from "../Aboutme.js";
+import Skills from "../Skills.js";
+import Achivements from "../Achivements.js";
+import { useState } from "react";
+import Experience from "../Experience.js";
+import Projects from "../Projects.js";
+import Contact from "../Contact.js";
+import Education from "../Education.js";
+import "../CssFiles/portfolio.css";
+import "../../index.css";
+
+const Preview = (props) => {
+  const data = props.data;
+  
+  return (
+    <>
+      <div className="Portfolio rounded-xl">
+          <main className="main">
+            <Home
+              name={data.name}
+              mainDesignations={data.mainDesignations}
+              description={data.description}
+              profilePicture={data.profilePicture}
+              githubProfile={data.githubProfile}
+              linkedIn={data.linkedIn}
+              instagram={data.instagram}
+            />
+            <hr />
+            <Aboutme
+              bio={data.bio}
+              yearsOfExperience={data.yearsOfExperience}
+              numberOfProjects={data.numberOfProjects}
+              profilePicture={data.profilePicture}
+            />
+            <hr />
+            <Education data={data.myEducation} />
+            <hr />
+            {data.myExperience.length ? (
+              <>
+                <Experience data={data.myExperience} />
+                <hr />
+              </>
+            ) : (
+              null
+            )}
+            {data.myProjects.length ? (<>
+                <Projects data={data.myProjects} />
+                <hr />
+              </>
+            ) : (
+              null
+            )}
+            <Skills data={data.mySkills} />
+            <hr />
+            <Achivements data={data.myAchievements} />
+            <hr />
+            <Contact
+              linkedIn={data.linkedIn}
+              instagram={data.instagram}
+              telephone={data.telephone}
+              email={data.email}
+            />
+          </main>
+      </div>
+    </>
+  );
+};
+
+export default Preview;
