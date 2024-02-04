83ed34dc7dbbac52ed70ef96b6eeff8c39a107ad
Devanshi <devanshigupta2002@gmail.com>
2024-02-04 21:13:19 +0530

    fix

--- source-folio/src/Components/Education.js
+++ source-folio/src/Components/Education.js
@@ -14,43 +14,49 @@ const Education=(props)=>{
           {
             props.data.map((x, i) => {
               return (
-              <div className="education__heading" style={(i > 0) ? { marginTop: "4rem" } : {}}>
-                <div className="education__name">
-                  <i
-                    className="education__pencil"
-                  >
-                    <BiPencil />
-                  </i>{" "}
-                  <h3
-                    className="education__college"
-                    style={{ color: "white", marginTop: "1.4rem" }}
-                  >
-                    <span>{x.institutionName}</span>
-                  </h3>
-                </div>
+                // <div className="education__heading" style={(i > 0) ? { marginTop: "4rem" } : {}}>
+                //   <div className="education__name">
+                //     <i
+                //       className="education__pencil"
+                //     >
+                //       <BiPencil />
+                //     </i>{" "}
+                //     <h3
+                //       className="education__college"
+                //       style={{ color: "white", marginTop: "1.4rem" }}
+                //     >
+                //       <span>{x.institutionName}</span>
+                //     </h3>
+                //   </div>
 
-                <h6 className="education__place">{x.place}</h6>
+                //   <h6 className="education__place">{x.place}</h6>
 
-                <div className="education__marks">
-                  <h4 style={{ color: "white" }}>
-                    <span className="education__detail">
-                      Aggregate: {x.aggregate} 
-                    </span>
-                    <span className="pipe"> | </span>
-                    <span className="education__detail">
-                      Course pursued: {x.coursePursuied}
-                    </span>
-                    <span className="pipe"> | </span>
-                    <span className="education__detail">
-                      Year: {x.year}
-                    </span>
-                  </h4>
+                //   <div className="education__marks">
+                //     <h4 style={{ color: "white" }}>
+                //       <span className="education__detail">
+                //         Aggregate: {x.aggregate}
+                //       </span>
+                //       <span className="pipe"> | </span>
+                //       <span className="education__detail">
+                //         Course pursued: {x.coursePursuied}
+                //       </span>
+                //       <span className="pipe"> | </span>
+                //       <span className="education__detail">
+                //         Year: {x.year}
+                //       </span>
+                //     </h4>
+                //   </div>
+                // </div>
+                <div class="flex flex-1">
+                  <p class="pr-6">Text</p>
+                  <div class="inline-block h-[250px] min-h-[1em] w-0.5 self-stretch bg-neutral-100 opacity-100 dark:opacity-50"></div>
+                  <p class="pl-6">Text</p>
                 </div>
-              </div>
               );
             })
           }
-
+          
+          
         </div>
       </section>
     );
