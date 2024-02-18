90032ae371827125407ad188fc3bb7376ac9ebea
Devanshi <devanshigupta2002@gmail.com>
2024-02-18 18:52:42 +0530

    added/education-uplift

--- source-folio/src/Components/Education.js
+++ source-folio/src/Components/Education.js
@@ -1,65 +1,55 @@
 import React from "react";
-import "./CssFiles/education.css"
-import { BiPencil } from "react-icons/bi";
+import "./CssFiles/education.css";
+// import { BiPencil } from "react-icons/bi";
 
-const Education=(props)=>{
-    return (
-      <section className="education section " id="education">
-        <h2 className="section__title">
-          My <span style={{ color: "orange" }}>Education.</span>
-        </h2>
-        <span className="section__subtitle">My education places</span>
+const Education = (props) => {
+  return (
+    <section className="education section " id="education">
+      <h2 className="section__title">
+        My <span style={{ color: "orange" }}>Education.</span>
+      </h2>
+      <span className="section__subtitle">My education places</span>
 
-        <div className="education__container nav__container">
-          {
-            props.data.map((x, i) => {
-              return (
-                // <div className="education__heading" style={(i > 0) ? { marginTop: "4rem" } : {}}>
-                //   <div className="education__name">
-                //     <i
-                //       className="education__pencil"
-                //     >
-                //       <BiPencil />
-                //     </i>{" "}
-                //     <h3
-                //       className="education__college"
-                //       style={{ color: "white", marginTop: "1.4rem" }}
-                //     >
-                //       <span>{x.institutionName}</span>
-                //     </h3>
-                //   </div>
-
-                //   <h6 className="education__place">{x.place}</h6>
-
-                //   <div className="education__marks">
-                //     <h4 style={{ color: "white" }}>
-                //       <span className="education__detail">
-                //         Aggregate: {x.aggregate}
-                //       </span>
-                //       <span className="pipe"> | </span>
-                //       <span className="education__detail">
-                //         Course pursued: {x.coursePursuied}
-                //       </span>
-                //       <span className="pipe"> | </span>
-                //       <span className="education__detail">
-                //         Year: {x.year}
-                //       </span>
-                //     </h4>
-                //   </div>
-                // </div>
-                <div class="flex flex-1">
-                  <p class="pr-6">Text</p>
-                  <div class="inline-block h-[250px] min-h-[1em] w-0.5 self-stretch bg-neutral-100 opacity-100 dark:opacity-50"></div>
-                  <p class="pl-6">Text</p>
-                </div>
-              );
-            })
-          }
-          
-          
+      <div>
+        <div className="flex items-center justify-center">
+          <div className="qualification__sections p-4">
+            <div className="qualification__content">
+              {props.data.map((x, i) => {
+                return (
+                  <div className="data_education" key={i}>
+                    <div className="w-16">
+                      <div className="font-bold">{x.year}</div>
+                    </div>
+                    <div>
+                      <div className="qualification__line"></div>
+                    </div>
+                    <div className="md:ml-24 ml-4">
+                      <div>
+                        <p className="w-full">
+                          <span className="qualification__rounder mr-2 text-sm p-2"></span>
+                          {x.institutionName} |{" "}
+                          <span className="text-xs font-bold">{x.place}</span>
+                        </p>
+                        <div>
+                          <span className="text-sm font-extrabold md:mr-2 p-1">
+                            Percentage - {x.aggregate}
+                          </span>{" "}
+                          |{" "}
+                          <span className="text-sm font-extrabold md:ml-2">
+                            Course - {x.coursePursuied}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
-      </section>
-    );
-}
+      </div>
+    </section>
+  );
+};
 
 export default Education;
