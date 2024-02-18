60a37459d1921f785b3f31a3017b84ef6fe5bf5f
Devanshi <devanshigupta2002@gmail.com>
2024-02-18 20:49:35 +0530

    added/education-section-changes

--- source-folio/src/Components/Education.js
+++ source-folio/src/Components/Education.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./CssFiles/education.css";
-// import { BiPencil } from "react-icons/bi";
 
 const Education = (props) => {
   return (
@@ -11,28 +10,32 @@ const Education = (props) => {
       <span className="section__subtitle">My education places</span>
 
       <div>
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center mr-2">
           <div className="qualification__sections p-4">
             <div className="qualification__content">
               {props.data.map((x, i) => {
                 return (
                   <div className="data_education" key={i}>
                     <div className="w-16">
-                      <div className="font-bold">{x.year}</div>
+                      <div className="font-bold text-gray-400">{x.year}</div>
                     </div>
                     <div>
                       <div className="qualification__line"></div>
                     </div>
-                    <div className="md:ml-24 ml-4">
+                    <div className="md:ml-24 ml-4 pb-5 w-[100%]">
                       <div>
                         <p className="w-full">
-                          <span className="qualification__rounder mr-2 text-sm p-2"></span>
-                          {x.institutionName} |{" "}
+                          <span className="education__rounder mr-2"></span>
+                          <span className="w-full text-gray-400">
+                            {" "}
+                            {x.institutionName}
+                          </span>{" "}
+                          |{"  "}
                           <span className="text-xs font-bold">{x.place}</span>
                         </p>
                         <div>
                           <span className="text-sm font-extrabold md:mr-2 p-1">
-                            Percentage - {x.aggregate}
+                            Aggregrate - {x.aggregate}
                           </span>{" "}
                           |{" "}
                           <span className="text-sm font-extrabold md:ml-2">
