651679a0415924ae57b0e51849530aa94b092036
Devanshi Gupta <devanshigupta2002@gmail.com>
2024-03-14 15:16:07 +0530

    Added/about-us

--- source-folio/src/Components/Dashboard-Components/AboutUs.js
+++ source-folio/src/Components/Dashboard-Components/AboutUs.js
@@ -1,46 +1,49 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-scroll";
 // import NavBar from "./DashNavbar";
 // import NavBar from "../NavBar";
-import { IoMdArrowRoundBack } from "react-icons/io";
+// import { IoMdArrowRoundBack } from "react-icons/io";
 
 const team = [
   {
-    name: "Michael Foster",
-    role: "Co-Founder / CTO",
+    name: "Sumit Verma",
+    role: "Backend Developer",
     imageUrl:
-      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+      "https://drive.google.com/thumbnail?id=1vELV7zyd9nwSOGeYzy8XzFWSnaKjz-uY&sz=w500",
   },
   {
-    name: "Michael Foster",
-    role: "Co-Founder / CTO",
+    name: "Devanshi Gupta",
+    role: "Frontend Developer",
+    imageUrl:"https://drive.google.com/thumbnail?id=1D3qMv-eYednp2ew8ShaBBibYfwHkeF7F&sz=w1000"  },
+  {
+    name: "Rahul Verma",
+    role: "Frontend Developer",
     imageUrl:
-      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+      "https://drive.google.com/thumbnail?id=1iXtEMStuvgrOplUuxbgpvP-ku3eGnRov&sz=w1000",
   },
   {
-    name: "Michael Foster",
-    role: "Co-Founder / CTO",
+    name: "Deepak Das",
+    role: "Backend Developer",
     imageUrl:
-      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "Michael Foster",
-    role: "Co-Founder / CTO",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+      "https://drive.google.com/thumbnail?id=1QMpoQ05yznD8fqc-Ovu0mTyMiBdr_ExV&sz=w1000",
   },
 ];
 
 const AboutUs = () => {
+
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="flex justify-center mt-6">
         <div className="flex md:w-1/2 justify-between w-full ml-4 mr-4">
-          <div>
-            SourceFolio&nbsp;<span style={{ color: "orange" }}>.</span>
-          </div>
-          <div>
+        <Link className="nav__logo" style={{ color: "white" }}>
+                SourceFolio&nbsp;<span style={{ color: "orange" }}>.</span>
+              </Link>
+          <div >
             {/* <IoMdArrowRoundBack className="text-white" /> */}
-            <p>Back to home</p>
+            <p className="text-white cursor-pointer nav__item" onClick={() => navigate(`/`)}>Back to home</p>
           </div>
         </div>
       </div>
@@ -48,23 +51,16 @@ const AboutUs = () => {
         <div className="max-w-7xl mx-auto">
           <div className="m-auto text-center md:ml-14">
             <div className="max-w-5xl lg:mx-0 text-center m-auto">
-              <div className="contentt">
-                <h2 className="text-3xl font-bold tracking-tight sm:text-4xl text-center text-white">
-                  OUR TEAM
-                </h2>
-                <h2 className="text-3xl font-bold tracking-tight sm:text-4xl text-center text-white">
+              <div>
+                <h2 className="tracking-tight md:text-6xl text-center text-white text-4xl aboutUsHeading font-black">
                   OUR TEAM
                 </h2>
               </div>
-              {/* <p className="mt-6 text-lg leading-8 text-gray-600 text-center">
-                Sit facilis neque ab nulla vel. Cum eos in laudantium.
-                Temporibus eos totam in dolorum. Nemo vel facere repellendus ut
-                eos dolores similique.
-              </p> */}
+              <p className="mt-6 text-lg leading-8 text-white text-center">
+                Meet our amazing team. We worked on sourceFolio together. Build your amazing portfolio with sourceFolio.
+              </p>
             </div>
-            <ul
-              role="list"
-              className="mt-20 grid max-w-2xl grid-cols-2 gap-x-12 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6 m-auto md:pl-40"
+            <ul className="mt-20 grid max-w-2xl grid-cols-2 gap-x-12 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6 m-auto md:pl-40"
             >
               {team.map((person) => (
                 <li key={person.name} className="text=center">
@@ -73,10 +69,10 @@ const AboutUs = () => {
                     src={person.imageUrl}
                     alt=""
                   />
-                  <h3 className="mt-6 text-base font-semibold tracking-tight text-gray-900">
+                  <h3 className="mt-6 text-base font-semibold tracking-tight text-white">
                     {person.name}
                   </h3>
-                  <p className="text-sm leading-6 text-gray-600">
+                  <p className="text-sm leading-6 text-gray-400">
                     {person.role}
                   </p>
                 </li>
